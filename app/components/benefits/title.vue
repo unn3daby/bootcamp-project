ae872b0d53bb96517055d2720ceedbad15228bd0
ilmarch1 <ilmarch1@mts.ru>
2026-05-28 11:56:38 +0300

    feat: i18n done

---
+++ app/components/benefits/title.vue
@@ -0,0 +1,31 @@
+<template>
+  <h2 class="font-bold xl:text-[50px] lg:text-[42px] text-[32px] leading-none mb-4">
+    <template v-for="(word, i) in words" :key="i">
+      <span
+        v-if="highlightSet.has(word)"
+        class="text-secondary"
+      >
+        {{ word }}
+      </span>
+
+      <span v-else>
+        {{ word }}
+      </span>
+
+      {{ i < words.length - 1 ? ' ' : '' }}
+    </template>
+  </h2>
+</template>
+
+<script setup lang="ts">
+const { text, highlighted = [] } = defineProps<{ text: string, highlighted?: string[] }>();
+const words = computed(() => text.split(' '));
+
+const highlightSet = computed(() =>
+  new Set(highlighted.map(word => word.toLowerCase())),
+);
+</script>
+
+<style scoped>
+
+</style>
