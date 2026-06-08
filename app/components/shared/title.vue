4ed506774213d52a8a4cae886dd142137e704e9f
ilmarch1 <ilmarch1@mts.ru>
2026-06-08 09:44:23 +0300

    feat: comments in title component

--- app/components/shared/title.vue
+++ app/components/shared/title.vue
@@ -1,8 +1,9 @@
 <template>
   <h2 class="font-bold xl:text-[50px] lg:text-[42px] text-[32px] leading-none mb-4">
+    <!-- итерируемся по массиву слов и навешиваем text-secondary, если слово есть в множестве -->
     <template v-for="(word, i) in words" :key="i">
       <span
-        v-if="highlightSet.has(word)"
+        v-if="highlightSet.has(word.toLowerCase())"
         class="text-secondary"
       >
         {{ word }}
@@ -12,6 +13,7 @@
         {{ word }}
       </span>
 
+      <!-- расставлем пробелы после слов, в конце оставляем пустую строку -->
       {{ i < words.length - 1 ? ' ' : '' }}
     </template>
   </h2>
